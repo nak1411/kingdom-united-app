bfd6ed1e06079568914612ad8c592c4c4fb873a7
Justin Schlump <nak1411@gmail.com>
2025-08-21 20:16:44 -0700

    cleanup

--- mobile/src/db/migrations/0001_add_indexes.sql
+++
@@ -1,4 +0,0 @@
--- Add indexes for better query performance
-CREATE INDEX IF NOT EXISTS "idx_data_user_id" ON "data" ("user_id");
-CREATE INDEX IF NOT EXISTS "idx_data_zip" ON "data" ("zip");
-CREATE INDEX IF NOT EXISTS "idx_data_created_at" ON "data" ("created_at");
